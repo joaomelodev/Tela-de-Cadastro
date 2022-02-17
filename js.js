033361ceb3c9704eece9beb4c4773941cd406cab
João Pedro <98651678+joaomelodev@users.noreply.github.com>
2022-02-17 11:01:04 -0300

    Add files via upload

---
+++ js.js
@@ -0,0 +1,10 @@
+let form = document.getElementById("register-form");
+
+let submit = document.getElementById("btn-subimit");
+
+submit.addEventListener('click', function(e) {
+ 
+    e.preventDefault();
+
+    console.log('funcionou');
+});
